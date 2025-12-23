06a8d9a2fe9d247c2e65471908016480f23605ad
ryomataichi <ryomaryoma0914@gmail.com>
2025-12-23 17:13:59 +0900

    （変更内容の説明）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tweets
+  get "oikons/top" => "oikons#index", as: :oikons_top 
   root 'oikons#index'
   get "tweets/match/:bangou" => "tweets#match", as: :tweets_match 
   get "tweets/leftmatch/:bangou" => "tweets#leftmatch", as: :tweets_leftmatch
